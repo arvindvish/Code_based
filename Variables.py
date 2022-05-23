f73efdeab1eb0f1535b0ee15cd291b65a98a8698
Arvind Vishwakarma <92598738+arvindvish@users.noreply.github.com>
2022-05-23 22:57:33 +0530

    Variable_python
    Variables_python

---
+++ Variables.py
@@ -0,0 +1,15 @@
+
+rent=2000
+gas=1300
+groceries=950
+
+print("House Rent:",rent)
+total=rent+gas+groceries
+print("Total expense:",total)
+
+item1="Rent"
+item2="gas"
+item3="groceries"
+item= item1,item2,item3
+
+print("Total expense item:",item)
