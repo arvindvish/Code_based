2c742f874665e0fc4f25e8cd823b7d57516fcdc1
Arvind Vishwakarma <92598738+arvindvish@users.noreply.github.com>
2022-05-24 14:16:04 +0530

    Numbers_python
    Numbers

---
+++ Numbers.py
@@ -0,0 +1,35 @@
+
+x=24+90
+y=15-7
+z=3-4
+a=3/4
+print (x,y,z,a)
+
+b= 11%2
+print(b)
+c=3**2
+d=2**4
+
+nyc_bal=188
+bal_pitt=247
+total_distance=nyc_bal+bal_pitt
+
+print(total_distance)
+mph=65
+time=total_distance/mph
+print(time)
+
+F=round(time,2)
+round(time,3)
+print(F)
+
+f=10+2+3
+print(f)
+ff=(10+2)*3
+print(ff)
+
+fff=6-5.7
+print(fff)
+
+dd=round(6-5.7,2)
+print(dd)
