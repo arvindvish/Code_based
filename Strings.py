ff8e0f3e3572627917eaad73a5c523154cfa4032
Arvind Vishwakarma <92598738+arvindvish@users.noreply.github.com>
2022-05-24 18:45:10 +0530

    Strings
    Strings_python

---
+++ Strings.py
@@ -0,0 +1,37 @@
+
+
+taxt = "ice cream"
+print(taxt)
+a=taxt[0]
+print(a)
+b=taxt[1]
+print(b)
+c= taxt[0:3]
+print(c)
+d=taxt[4:9]
+print(d)
+e=taxt[4:]
+print(e)
+f=taxt[:3]
+print(f)
+
+taxt="hello"
+taxt='hello'
+taxt="let's learn python"
+taxt='hello"world"'
+print(taxt)
+
+adress= '''1 purple 2nd street
+new york
+usa'''
+print(adress)
+
+a1="hello"
+a2="world"
+print(a1+a2)
+print(a1+' '+a2)
+
+s="total street in usa # "
+num=25
+str(num)
+print(s+str(num))
